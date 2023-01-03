cc75aa88624fae25b576ce73fc542e85387667e5
Pankratov <pnrf@yandex.ru>
2023-01-03 09:55:07 +0300

    feat: add pageNotFound

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,8 +1,16 @@
 import './PageNotFound.css';
+import { useNavigate } from 'react-router-dom';
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
   return (
-    <div className="page-nf">
+    <div className="page-not-found">
+      <div className="page-not-found__container">
+        <h1 className="page-not-found__title">404</h1>
+        <p className="page-not-found__text">Страница не найдена</p>
+      </div>
+      <button onClick={() => navigate(-1)} className="page-not-found__link">Назад</button>
     </div>
   );
 };
